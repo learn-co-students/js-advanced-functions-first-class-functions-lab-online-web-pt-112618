78bb9e11a404cd1c39b3603733cfc07eb30a9264
Chris Philbin <chris@chrisphilbin.net>
2019-06-20 16:12:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let returnFirstTwoDrivers = function(driversArray) {
 	return driversArray.slice(0,2)
 }
+
+let returnLastTwoDrivers = function(driversArray) { 
+	return driversArray.slice((driversArray.length - 2), driversArray.length)
+}
